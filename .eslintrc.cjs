6f25438f6b73cb500499b673d86bfc3a6d1586dc
Mark <m4rc0d3r@gmail.com>
2023-05-19 12:45:00 +0300

    Set up linting rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,45 @@
 /* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
+require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
-  'extends': [
-    'plugin:vue/vue3-essential',
-    'eslint:recommended',
-    '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+  "extends": [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-typescript",
+    "@vue/eslint-config-prettier/skip-formatting",
   ],
   parserOptions: {
-    ecmaVersion: 'latest'
-  }
-}
+    ecmaVersion: "latest",
+  },
+  "rules": {
+    "indent": [
+      "error",
+      2,
+    ],
+    "linebreak-style": [
+      "error",
+      "unix",
+    ],
+    "quotes": [
+      "error",
+      "double",
+    ],
+    "semi": [
+      "error",
+      "always",
+    ],
+    "object-curly-spacing": [
+      "error",
+      "always",
+    ],
+    "comma-dangle": [
+      "error",
+      "always-multiline",
+    ],
+    "max-len": [
+      "error",
+      { "tabWidth": 2 },
+    ],
+  },
+};
